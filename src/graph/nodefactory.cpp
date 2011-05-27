b6afae760634b4379638aa933f0cd4f0a30d9e4e
Spencer Jackson <spencerandrewjackson@gmail.com>
2011-05-27 19:54:32 -0400

    Allow deserialization of packages to Node objects

---
+++ src/graph/nodefactory.cpp
@@ -0,0 +1,33 @@
+/*
+    Hive: A robotics package manager
+    Copyright (C) 2011  Spencer Jackson <spencerandrewjackson@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "nodefactory.h"
+
+#include "node.h"
+
+#include "graph/protocolbuffers/packagedescription.pb.h"
+
+namespace hive {
+
+std::shared_ptr<Node> NodeFactory::construct_node(PackageDescription const& package) {
+	std::shared_ptr<Node> ret{ new Node{package.name()}};
+	return ret;
+}
+
+
+} //hive
